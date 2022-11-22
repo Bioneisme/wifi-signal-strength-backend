dcc23883813b249ab0187b870b027d2f32ed8152
Bioneisme <bioneisme@gmail.com>
2022-11-22 22:24:19 +0600

    Added generate jwt

--- helpers/jwt.ts
+++ helpers/jwt.ts
@@ -1,10 +1,9 @@
 import {JWT_SECRET} from "../config/settings";
-import {Types} from "mongoose";
 import jwt, {JwtPayload} from "jsonwebtoken";
 
 type Decoded = { id: string } | string | JwtPayload;
 
-export function generateJWT(id: Types.ObjectId): string {
+export function generateJWT(id: number): string {
     return jwt.sign({id}, JWT_SECRET, {
         expiresIn: "1d",
     });
