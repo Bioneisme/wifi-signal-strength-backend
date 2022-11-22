73e02e5158a395b556d7b998016543d01d154ea5
Bioneisme <bioneisme@gmail.com>
2022-11-22 19:08:27 +0600

    Added dotenv import

--- config/settings.ts
+++ config/settings.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 // const NODE_ENV: string = process.env.NODE_ENV || 'development';
 
 export const MONGO_URI: string = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
