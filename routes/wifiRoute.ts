85ec3ab911809c6966db43551658928931daea83
Bioneisme <bioneisme@gmail.com>
2022-12-20 19:57:47 +0600

    Changed to get from db

--- routes/wifiRoute.ts
+++ routes/wifiRoute.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import {deleteWifi, getUserWifi, getWifi, getWifis, postWifi} from "../controllers/wifiController";
 import test from "./test.json";
+import {Test} from "../entities";
+import {DI} from "../index";
 
 const router: Router = Router();
 
@@ -14,9 +16,11 @@ router.delete("/deleteWifi/:id", deleteWifi);
 
 router.get("/getPhones/:id", async (req, res) => {
     const {id} = req.params;
-    // @ts-ignore
-    let phones = test.slice(0, id);
-    // const phones = await DI.em.find(Test, {}, {fields: ["name", "phone"]});
+    const phones = await DI.em.find(Test, {
+        id: {
+            $gt: +id
+        }
+    }, {fields: ["name", "phone"]});
     res.send(phones);
 });
 
